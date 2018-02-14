6e2cf8c21c2a75671bb203e7b318a604342a37bc
Salvador Juan Martinez <saljuama@gmail.com>
2018-02-15 00:16:20 +0100

    solved AboutWithStatement

--- koans/about_with_statements.py
+++ koans/about_with_statements.py
@@ -9,7 +9,9 @@ from runner.koan import *
 
 import re # For regular expression string comparisons
 
+
 class AboutWithStatements(Koan):
+
     def count_lines(self, file_name):
         try:
             file = open(file_name)
@@ -22,7 +24,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_counting_lines(self):
-        self.assertEqual(__, self.count_lines("example_file.txt"))
+        self.assertEqual(4, self.count_lines("example_file.txt"))
 
     # ------------------------------------------------------------------
 
@@ -41,7 +43,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_finding_lines(self):
-        self.assertEqual(__, self.find_line("example_file.txt"))
+        self.assertEqual('test\n', self.find_line("example_file.txt"))
 
     ## ------------------------------------------------------------------
     ## THINK ABOUT IT:
@@ -85,17 +87,20 @@ class AboutWithStatements(Koan):
             return len(file.readlines())
 
     def test_counting_lines2(self):
-        self.assertEqual(__, self.count_lines2("example_file.txt"))
+        self.assertEqual(4, self.count_lines2("example_file.txt"))
 
     # ------------------------------------------------------------------
 
     def find_line2(self, file_name):
         # Rewrite find_line using the Context Manager.
-        pass
+        with self.FileContextManager(file_name) as file:
+            for line in file.readlines():
+                if re.search('e', line):
+                    return line
 
     def test_finding_lines2(self):
-        self.assertEqual(__, self.find_line2("example_file.txt"))
-        self.assertNotEqual(__, self.find_line2("example_file.txt"))
+        self.assertEqual('test\n', self.find_line2("example_file.txt"))
+        self.assertNotEqual('anything but test\n', self.find_line2("example_file.txt"))
 
     # ------------------------------------------------------------------
 
@@ -104,4 +109,4 @@ class AboutWithStatements(Koan):
             return len(file.readlines())
 
     def test_open_already_has_its_own_built_in_context_manager(self):
-        self.assertEqual(__, self.count_lines3("example_file.txt"))
+        self.assertEqual(4, self.count_lines3("example_file.txt"))
